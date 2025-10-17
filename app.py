6248ab8b8cc16c30dff87c2f8c69a9699055dad8
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-10-17 18:52:24 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,105 @@
+import os
+import sys
+from datetime import datetime
+from time import sleep
+
+import streamlit as st
+
+sys.path.append(r"..\aapn_ur")
+
+from auth import AuthManager
+
+# Configuración de página con fondo personalizado
+st.set_page_config(
+    page_title="Nuevo creyente",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    page_icon="",
+)
+
+MENU_INICIO = "pages/page1.py"
+
+st.title("Inicio de sesión")
+
+# Cargar las claves de session si no existen
+for key, default in [
+    ("stage", 0),
+    ("conexion", None),
+    ("auth_manager", None),
+]:
+    if key not in st.session_state:
+        st.session_state[key] = default
+
+
+def set_stage(i):
+    st.session_state.stage = i
+
+
+if st.session_state.stage == 0:
+    from dotenv import load_dotenv
+
+    sys.path.append(r"..\conexiones")
+    from conn.database_connector import DatabaseConnector
+    from conn.mysql_connector import MySQLConnector
+
+    load_dotenv(override=True)
+
+    mysql_connector = MySQLConnector(
+        host=st.secrets.auth.DB_HOST,
+        database=st.secrets.auth.DB_NAME,
+        user=st.secrets.auth.DB_USER_ADMIN,
+        password=st.secrets.auth.DB_PASSWORD,
+    )
+
+    mysql_connector.connect()
+    st.session_state.conexion = DatabaseConnector(mysql_connector)
+    st.session_state.auth_manager = AuthManager(st.session_state.conexion)
+    set_stage(1)
+
+
+def existe_user(username):
+    return st.session_state.auth_manager.user_existe(username)
+
+
+def login(user, passw):
+    return st.session_state.auth_manager.autenticar(user, passw)
+
+
+def iniciar_sesion(user):
+    st.session_state.logged_in = True
+    st.session_state.user = user
+    st.switch_page(MENU_INICIO)
+
+
+if st.session_state.stage == 1:
+    if "usuario" not in st.session_state:
+        st.write("Por favor ingrese su usuario.")
+        # Si el usuario aún no ha sido ingresado
+        user = st.text_input("Usuario:")
+        if existe_user(user):
+            st.session_state.usuario = user
+            st.success("Usuario validado!")
+            sleep(1)
+            st.rerun()
+        else:
+            if user:
+                st.error("El usuario no existe. Inténtalo de nuevo.")
+    else:
+        # Si el usuario ya ha sido ingresado, se oculta el input y se muestra el usuario ingresado
+        st.write(f"### Usuario ingresado: {st.session_state.usuario}")
+
+        # Pedir la contraseña
+        password = st.text_input("Ingresa tu contraseña:", type="password")
+
+        if st.button("Log in", type="primary"):
+            flag, msg = login(user=st.session_state.usuario, passw=password)
+
+        if password:
+            flag, msg = login(user=st.session_state.usuario, passw=password)
+            if not flag:
+                st.toast(msg, icon="⚠️")
+                sleep(1)
+                password = ""
+            else:
+                st.toast(msg, icon="✅")
+                iniciar_sesion(st.session_state.usuario)
